9f6974900ac388e271bb145d1cfb8f0eeafb3e23
Carson Gross <carson@bigsky.software>
2022-05-18 17:26:31 -0600

    reshuffle pdfs into a sub-director and create a shell script to help generate them

---
+++ manuscript/bin/gen-pdfs.sh
@@ -0,0 +1,4 @@
+asciidoctor-pdf -o pdfs/chapter_1_hypermedia.pdf chapter_1_hypermedia.adoc
+asciidoctor-pdf -o pdfs/chapter_2_rest_hateoas_etc.pdf chapter_2_rest_hateoas_etc.adoc
+asciidoctor-pdf -o pdfs/chapter_3_contact_app.pdf chapter_3_contact_app.adoc
+asciidoctor-pdf -o pdfs/book.pdf index.adoc

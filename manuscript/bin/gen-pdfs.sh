7e6bcc417e22a2c35a02adb0cfff08f0e25903bc
Carson Gross <carson@bigsky.software>
2022-06-02 15:34:42 -0600

    move chapter 3 to chapter 2, move fielding stuff to an appendix, reorg root files for consistency

--- manuscript/bin/gen-pdfs.sh
+++ manuscript/bin/gen-pdfs.sh
@@ -1,5 +1,4 @@
-asciidoctor-pdf -o pdfs/generated/CH01_gross_Hypermedia_In_Action.pdf chapter_1_hypermedia.adoc
-asciidoctor-pdf -o pdfs/generated/CH02_gross_Hypermedia_In_Action.pdf chapter_2_rest_hateoas_etc.adoc
-asciidoctor-pdf -o pdfs/generated/CH03_gross_Hypermedia_In_Action.pdf chapter_3_contact_app.adoc
+asciidoctor-pdf -o pdfs/generated/CH01_gross_Hypermedia_In_Action.pdf CH01_gross_hypermedia.adoc
+asciidoctor-pdf -o pdfs/generated/CH02_gross_Hypermedia_In_Action.pdf CH02_gross_creating_a_web_app.adoc
 asciidoctor-pdf -o pdfs/generated/Hypermedia_In_Action.pdf index.adoc
 asciidoctor-pdf -o pdfs/generated/TOC_gross_Hypermedia_In_Action.pdf TOC.adoc

4d4a07205602e53c287a4ab29afe07887c3a9d8c
Carson Gross <carson@bigsky.software>
2022-06-08 07:27:28 -0600

    chapter 3 + pdfs

--- manuscript/bin/gen-pdfs.sh
+++ manuscript/bin/gen-pdfs.sh
@@ -1,4 +1,5 @@
 asciidoctor-pdf -o pdfs/generated/CH01_gross_Hypermedia_In_Action.pdf CH01_gross_hypermedia.adoc
 asciidoctor-pdf -o pdfs/generated/CH02_gross_Hypermedia_In_Action.pdf CH02_gross_creating_a_web_app.adoc
+asciidoctor-pdf -o pdfs/generated/CH03_gross_Hypermedia_In_Action.pdf CH03_gross_extending_html.adoc
 asciidoctor-pdf -o pdfs/generated/Hypermedia_In_Action.pdf index.adoc
 asciidoctor-pdf -o pdfs/generated/TOC_gross_Hypermedia_In_Action.pdf TOC.adoc

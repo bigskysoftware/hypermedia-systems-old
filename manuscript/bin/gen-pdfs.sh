af0df21200efffedebd59bb62a56607db2e2f654
Carson Gross <carson@bigsky.software>
2022-07-18 16:46:49 -0600

    Chapter 4 cleanup

--- manuscript/bin/gen-pdfs.sh
+++ manuscript/bin/gen-pdfs.sh
@@ -1,7 +1,7 @@
-asciidoctor-pdf -o pdfs/generated/CH01_gross_Hypermedia_In_Action.pdf CH01_gross_hypermedia.adoc
-asciidoctor-pdf -o pdfs/generated/CH02_gross_Hypermedia_In_Action.pdf CH02_gross_creating_a_web_app.adoc
-asciidoctor-pdf -o pdfs/generated/CH03_gross_Hypermedia_In_Action.pdf CH03_gross_extending_html.adoc
-asciidoctor-pdf -o pdfs/generated/CH04_gross_Hypermedia_In_Action.pdf CH04_improving_our_hypermedia_application.adoc
-asciidoctor-pdf -o pdfs/generated/CH05_gross_Hypermedia_In_Action.pdf CH05_advanced_hypermedia_patterns.adoc
-asciidoctor-pdf -o pdfs/generated/Hypermedia_In_Action.pdf index.adoc
-asciidoctor-pdf -o pdfs/generated/TOC_gross_Hypermedia_In_Action.pdf TOC.adoc
+asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/CH01_gross_Hypermedia_In_Action.pdf CH01_gross_hypermedia.adoc
+asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/CH02_gross_Hypermedia_In_Action.pdf CH02_gross_creating_a_web_app.adoc
+asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/CH03_gross_Hypermedia_In_Action.pdf CH03_gross_extending_html.adoc
+asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/CH04_gross_Hypermedia_In_Action.pdf CH04_improving_our_hypermedia_application.adoc
+asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/CH05_gross_Hypermedia_In_Action.pdf CH05_advanced_hypermedia_patterns.adoc
+asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/Hypermedia_In_Action.pdf index.adoc
+asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/TOC_gross_Hypermedia_In_Action.pdf TOC.adoc

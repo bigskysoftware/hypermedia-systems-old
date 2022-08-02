3845b16885107c673f86c947553bd339aceb13d6
Carson Gross <carson@bigsky.software>
2022-08-02 15:38:39 -0600

    chapter 9 1st pass

--- manuscript/bin/gen-pdfs.sh
+++ manuscript/bin/gen-pdfs.sh
@@ -3,5 +3,6 @@ asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/CH02_gross_Hypermedia_In_
 asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/CH03_gross_Hypermedia_In_Action.pdf CH03_gross_extending_html.adoc
 asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/CH04_gross_Hypermedia_In_Action.pdf CH04_improving_our_hypermedia_application.adoc
 asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/CH05_gross_Hypermedia_In_Action.pdf CH05_advanced_hypermedia_patterns.adoc
+asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/CH09_gross_Hypermedia_In_Action.pdf CH09_gross_json_data_apis.adoc
 asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/Hypermedia_In_Action.pdf index.adoc
 asciidoctor-pdf -a pdf-theme=manning -o pdfs/generated/TOC_gross_Hypermedia_In_Action.pdf TOC.adoc
